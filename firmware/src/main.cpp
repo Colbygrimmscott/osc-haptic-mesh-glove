4e23495969531725ea9d1aec4c2c0f2a1081abff
Colbygrimmscott <colbygrimmscott@ufl.edu>
2026-06-25 14:09:25 -0400

    Added basic I2C device scan code and serial output for testing initial setup.

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,9 +1,44 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
 
+// Initial setup when turned on
 void setup() {
+    Wire.begin();
     Serial.begin(115200);
+    while (!Serial) delay(10);
+    Serial.println("16 channel PWM test");
 }
 
+void scanSerialDevices(int *numDevices) {
+    byte error, address, prevAddress;
+    prevAddress = 0;
+    for (address = 1; address < 127; address++) {
+        Wire.beginTransmission(address);
+        error = Wire.endTransmission();
+        if (error == 0) {
+            Serial.print("I2C device found at address 0x");
+            Serial.println(address, HEX);
+            prevAddress = address;
+            (*numDevices)++;
+        }
+        else if (error == 4) {
+            Serial.print("Unknown error at address 0x");
+            Serial.println(address, HEX);
+        }
+    }
+
+    if (*numDevices == 0) {
+        Serial.println("No I2C devices found\n");
+    }
+}
+
+
+
 void loop() {
-    
+    int numDevices = 0;
+    scanSerialDevices(&numDevices);
+
+    Serial.println("----------");
+    delay(500);
 }
