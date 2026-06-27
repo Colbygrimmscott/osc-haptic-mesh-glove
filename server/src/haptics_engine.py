7033ab62430fbf527e1067794483cc772c6e044b
Colbygrimmscott <colbygrimmscott@ufl.edu>
2026-06-27 12:44:18 -0400

    Added simple haptic engine class with placeholder function to retrieve motor values.

---
+++ server/src/haptics_engine.py
@@ -0,0 +1,15 @@
+from typing import List, Any
+
+class HapticsEngine:
+    def __init__(self):
+        self.values = None
+        self.prev_values = None
+
+    def _set_values(self, *osc_arguments: List[Any]):
+        self.values = osc_arguments
+
+    def _get_values(self):
+        data_list = []
+        for i in range(0, 30):
+            data_list.append(0.01 + i/30)
+        return data_list
