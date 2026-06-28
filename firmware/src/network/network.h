2ef8e0db4dcb8072d1cac4fd418e31d4bf2e1c6e
Colbygrimmscott <colbygrimmscott@ufl.edu>
2026-06-28 13:55:26 -0400

    Added new custom struct for glove controller state packets and added new function prototype for sending a packet using gloveController class.

--- firmware/src/network/network.h
+++ firmware/src/network/network.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <WifiUdp.h>
+#include "controller/controller.h"
 
 /**
  * Custom packet struct for deciphering incoming haptic motor control packets from PC server
@@ -10,6 +11,19 @@ struct __attribute__((__packed__)) HapticMotorPacket {
 };
 
 /**
+ * Custom packet struct for sending controller data to PC server.
+ */
+struct __attribute__((__packed__)) GloveControlPacket {
+    uint16_t header;                ///< Identifying header used to verify packet target.
+    uint16_t joyStickXVal;          ///< X value of joystick
+    uint16_t joyStickYVal;          ///< Y value of joystick
+    uint8_t joyStickButtonVal;      ///< State of joystick button
+    uint8_t buttonBotVal;           ///< State of bottom button
+    uint8_t buttonTopVal;           ///< State of top button
+};
+
+
+/**
  * Manages UDP network connection and packet parsing
  */
 class EspUdpNetwork {
@@ -18,6 +32,7 @@ private:
     HapticMotorPacket incomingPacket;   ///< Stores most recent packet.
     WiFiUDP udpConnection;              ///< Used to initialize and connect to the internet using UDP.
     int packetSize;                     ///< Stores the size of most recent packet.
+    gloveController gloveControllerObj;
 
 public:
 //  EspUdpNetwork();
@@ -33,4 +48,6 @@ public:
      * Copies data for motor intensities into an array passed as input
      */
     void parseIncomingPacket(uint8_t* outputMotorIntensities, int arraySize);
+
+    void sendControlPacket()
 };
