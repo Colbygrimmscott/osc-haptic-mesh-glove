db9a364783b07c3a3e68a28080d6e7230bfbc364
Colbygrimmscott <colbygrimmscott@ufl.edu>
2026-06-28 13:04:59 -0400

    Added static IP address, gateway and subnet to config for temporary hardcoded connection.

--- firmware/include/config.h
+++ firmware/include/config.h
@@ -1,8 +1,16 @@
 #pragma once
+#include <WiFi.h>
 
 // Define wi-fi credentials (UNCOMMENT)
 // const char* ssid = "ENTER_WIFI_SSID";
 // const char* wifiPassword = "ENTER_WIFI_PASSWORD";
+
+const IPAddress localIP(192, 168, 4, 48);
+const IPAddress gateway(192, 168, 1, 1);
+const IPAddress subnet(255, 255, 255, 0);
+
+// Define glove port for UDP connection
+const uint16_t glovePort = 4242;
 
 // Define PWM I2C addresses
 constexpr int pwm1Address = 0x40;
@@ -16,5 +24,3 @@ constexpr int joyStickButton = 6;
 constexpr int buttonBot = 7;
 constexpr int buttonTop = 8;
 
-// Define glove port for UDP connection
-constexpr int glovePort = 4242;
